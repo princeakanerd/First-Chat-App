d3742d22e72b8bee28c02cfcbfecbb7ac5bb44c5
princeakanerd <princeakanerd@gmail.com>
2025-04-01 21:57:29 +0530

    donee

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,5 +5,5 @@ const app = express() ;
 
 app.use("/api/auth" , authRoutes)
 app.listen(5001 , () => {
-    console.log("serverr is running  on port 5001")
+    console.log("serverr is runing  on port 5001")
 });
