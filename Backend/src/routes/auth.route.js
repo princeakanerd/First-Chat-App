bc3fa52904393b9da03c8f71084003e97f771c88
princeakanerd <princeakanerd@gmail.com>
2025-04-03 00:20:45 +0530

    Built User model js

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express" ;
 import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = express.Router() ;
 
-router.get("/signup", signup)
+router.post("/signup", signup)
 router.post("/login", login)
 router.post("/logout", logout)
 export default router ;
