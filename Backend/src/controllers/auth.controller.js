af64642b632156fff430c3c46b60cf1868e4e593
princeakanerd <princeakanerd@gmail.com>
2025-04-04 23:37:55 +0530

    learning JWT

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,18 @@
-export const signup = (req , res)=> {
-    res.send("signup route")
+import User from "../models/user.model.js"
+export const signup = async (req , res)=> {
+    const{fullname , email , password} = req.body; 
+    try {
+       if(password.length < 6) {
+        return res.status(400).json({message: "Password must be atleast 5 characters"}) ;
+       }
+       const user = await User.findOne({email}) ;
+       if(user) return res.status(400).jsonn({message : "Email already exists"}) ;
+       
+       const salt = await bc 
+        
+    } catch (error) {
+        
+    }
 }
 export const login = (req , res)=> {
     res.send("login route")
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,6 +7,7 @@ dotenv.config() ;
 const app = express() ;
 
 const PORT = process.env.PORT
+app.use(express.json())
 app.use("/api/auth" , authRoutes)
 app.listen(PORT , () => {
     console.log("serve is runing  on port: " + PORT)
