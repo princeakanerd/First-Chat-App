b7582d446d2900984eaf2c08b6ff537ad5e8194b
princeakanerd <princeakanerd@gmail.com>
2025-04-13 00:09:09 +0530

    Postman

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,17 +1,40 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 export const signup = async (req , res)=> {
-    const{fullname , email , password} = req.body; 
+    const{fullName , email , password} = req.body; 
     try {
        if(password.length < 6) {
-        return res.status(400).json({message: "Password must be atleast 5 characters"}) ;
+        return res.status(400).json({message: "Password must be atleast 6 characters"}) ;
        }
        const user = await User.findOne({email}) ;
        if(user) return res.status(400).jsonn({message : "Email already exists"}) ;
        
-       const salt = await bc 
+       const salt = await bcrypt.genSalt(10) ;
+       const hashedPassword = await bcrypt.hash(password , salt) ;
         
+       const newUser = new User({
+           fullName : fullName,
+           email : email ,
+           password: hashedPassword
+       }
+       )
+
+       if(newUser){
+        generateToken(newUser._id , res)
+        await newUser.save() ;
+        res.status(201).json({
+            _id : newUser._id , 
+            fullName : newUser.fullName , 
+            email : newUser.email , 
+            profilePic : newUser.profilePic ,
+        })
+       }else {
+        res.status(400).json({message : "invalid User data" })
+       }
     } catch (error) {
-        
+        console.log("Error in signup controller" , error.message) ;
+        res.status(500).json({message : "Internal Server Error"}) ;
     }
 }
 export const login = (req , res)=> {
--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
     {
         email :{
-            type:string ,
+            type: String ,
             required : true , 
             unique : true , 
         } , 
