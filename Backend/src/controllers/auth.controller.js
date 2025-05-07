6c6812b031afdfed8e06928c957ccdca514178c0
princeakanerd <princeakanerd@gmail.com>
2025-05-07 17:14:02 +0530

    Cloudinary Used

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -39,9 +39,41 @@ export const signup = async (req , res)=> {
         res.status(500).json({message : "Internal Server Error"}) ;
     }
 }
-export const login = (req , res)=> {
-    res.send("login route")
+export const login = async (req , res)=> {
+    const {email , password} = req.body
+
+    try {
+        const user = await User.findOne({email}) ;
+
+        if(!user) return res.status(400).json({message : "invalid Credentials"}) ;
+        
+        const isPasswordCorrect = await bcrypt.compare(password , user.password) ;
+
+        if(!isPasswordCorrect) return res.status(400).json({message : "Invalid credentials"}) ;
+
+        generateToken(user._id , res) ;
+
+        res.status(200).json({
+            _id:user._id ,
+            fullName : user.fullName ,
+            email: user.email ,
+            profilePic : user.profilePic ,
+        })
+    } catch (error) {
+        console.log("Error in login controller" , error.message) ;
+        res.status(500).json({message : "server error"}) ;
+    }
 }
 export const logout = (req , res)=> {
-    res.send("logout route")
+    try {
+        res.cookie("jwt", "" , {maxAge : 0}) ;
+        res.status(200).json({message : "Logged out succesfully"}) ;
+    } catch (error) {
+        console.log("Error in logout controller" , error.message) ;
+        res.status(500).json({message : "server error"}) ;
+    }
+}
+
+export const updateProfile = async (req , res) => {
+
 }
---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+//Iska specific purpose hi wo protect route banana tha
+
+import jwt from "jsonwebtoken"
+//We want to validate the token hence we have added this import
+import User from "../models/user.model.js"
+// Auth is related to the user so
+
+export const protectRoute = async (req , res , next) => {
+    try {
+        const token = req.cookie.jwt 
+        //idhar jwt isiliye kyunki utils.js mein usko maine ye naam dia tha
+        if(!token) {
+            return res.status(401).json({message : "No Tokens provided bro"}) ;
+        }
+        
+        const decoded = jwt.verify(token , process.env.JWT_SECRET) ;
+        if(!decoded){
+            return res.status(401).json({message : "Unauthorised , Invalid token "}) ;
+        }
+
+        const user = await User.findById(decoded.userId).select("-password") ;
+        if(!user){
+            return res.status(404).json({message :"user not found"}) ;
+        }
+
+        req.user = user ;
+        next()
+
+    } catch (error) {
+        console.log("Some errorrr" , error.message) ;
+        res.status(500).json({message : "Internal server error"}) ;
+    }
+}
