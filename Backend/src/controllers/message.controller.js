efe5a75e38237c31df05737cf747022e5f5b2e49
princeakanerd <princeakanerd@gmail.com>
2025-05-08 23:08:25 +0530

    BackEnd Nearly complete

---
+++ Backend/src/controllers/message.controller.js
@@ -0,0 +1,75 @@
+import User from "../models/user.model.js"
+import Message from "../models/message.model.js"
+import cloudinary from "../lib/cloudinary.js";
+
+export const getUsersForSidebar = async (req , res) => {
+    try{
+        //As we used protectRoute hence we can fetch user id from req.user 
+        const loggedInUserId = req.user._id ;
+        //ne is not equal , It fetches all the users whose id is != to loggedUserId
+        //Just dont send the password
+        const filteredUsers = await User.find({_id : {$ne:loggedInUserId}}).select("-password") ;
+        res.status(200).json(filteredUsers) ;
+    }catch(error){
+        console.error("Error in getUsersForSidebar" , error.message) ;
+        res.status(500).json({error : "Error in getUsersForSidebar"})
+    }
+}
+
+export const getMessages = async (req , res) => {
+
+    try {
+        //router.get("/:id",protectRoute, getMessages)
+        // Ye upar waali line mein :id tha isiliye niche bhi id hi likhna pada , Ye upar wali line is a part of mess.routes
+        const { id : userToChatId} = req.params ;
+        const myId = req.user._id ;
+
+        //these are allx the messages where sender is me and receiver is other user else receiver is me and sender is other user
+        // Hum 2 log ke beech ke saare messages nikaal rhe hain
+        const messages = await Message.find({
+            $or:[
+                {senderId : myId , recerverId : userToChatId},
+                {senderId : userToChatId , recerverId : myId},
+
+            ]
+        })
+
+        res.status(200).json(messages) ;
+    } catch (error) {
+        console.log("Error in getMessages" , error.message);
+        res.status(500).json({error :" Error in GetMessages"}) ;
+
+    }
+}
+
+export const sendMessage = async(req ,res) => {
+    //this message could be a text or an image
+
+    try {
+        const {text , image} = req.body ;
+        const {id :receiverId} = req.params ;
+        const senderId = req.user._id ;
+
+        let imageUrl ;
+        if(image) {
+            //upload base64 image to cloudinary 
+            const uploadResponse = await cloudinary.uploader.upload(image) ;
+            imageUrl = uploadResponse.secure_url ;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image : imageUrl ,
+        });
+
+        await newMessage.save() ;
+        // todo : realTime functionality using socket io
+
+        res.status(200).json(newMessage) ;
+    } catch (error) {
+        console.log("Error in sendMessage controller" , error.message) ;
+        res.status(500).json({error : "Error in sendMessage Controller"}) ;
+    }
+}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,14 +3,17 @@ import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js" ;
 import authRoutes from './routes/auth.route.js';
 import cookieParser from "cookie-parser" ;
-
+import messageRoutes from "./routes/message.route.js"
 dotenv.config() ;
 const app = express() ;
 
 const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth" , authRoutes)
+app.use("/api/message" , messageRoutes)
+
 app.listen(PORT , () => {
     console.log("serve is runing  on port: " + PORT)
     connectDB();
